9adaabae83c17990a45170553ae5075c569c4c01
Austen LeBeau <austenlebeau@gmail.com>
2019-05-03 16:15:12 -0500

    still working on problem 2

--- final/src/main.cc
+++ final/src/main.cc
@@ -3,82 +3,57 @@
  * ENGR 3310-002
  */
 
+#include <algorithm>
 #include <iomanip>
 #include <iostream>
-#include <algorithm>
-#include "lambert.h"
 #include "date.h"
+#include "lambert.h"
 #include "orbitable.h"
 #include "util.h"
-
 
 #define AU 1.49597870700e11
 #define RAD2DEG(x) x*(180.0 / M_PI)
 
 int main() {
-
     std::cout << std::scientific;
 
-    Orbitable Earth (
-            Vector (
-                -8.601208063143356e-1,
-                -5.149780543253561e-1,
-                -7.874942568326342e-6
-                ),
-            Vector (
-                8.657167839482775e-3,
-                -1.475977839094277e-2,
-                8.628767650706375e-7
-                )
-            );
+    Orbitable Earth(Vector(-8.601208063143356e-1, -5.149780543253561e-1,
+                           -7.874942568326342e-6),
+                    Vector(8.657167839482775e-3, -1.475977839094277e-2,
+                           8.628767650706375e-7));
 
-    Orbitable Mars (
-            Vector (
-                -2.622004974599996e-1,
-                1.577561813000981,
-                3.925526885339471e-2
-                ),
-            Vector (
-                -1.328446880856093e-2,
-                -1.101073656563134e-3,
-                3.028803664779320e-4
-                )
-            );
+    Orbitable Mars(
+        Vector(-2.622004974599996e-1, 1.577561813000981, 3.925526885339471e-2),
+        Vector(-1.328446880856093e-2, -1.101073656563134e-3,
+               3.028803664779320e-4));
 
     // Asteroid
-    Orbitable ToshiH (
-            Vector (
-                -7.436280142422779e-1,
-                -2.774619548644846,
-                1.476905508096402e-1
-                ),
-            Vector (
-                8.942035174142673e-3,
-                -3.065578719141641e-3,
-                9.533854781906894e-4
-                )
-            );
+    Orbitable ToshiH(
+        Vector(-7.436280142422779e-1, -2.774619548644846, 1.476905508096402e-1),
+        Vector(8.942035174142673e-3, -3.065578719141641e-3,
+               9.533854781906894e-4));
 
-    Gregorian current_gregorian (2019, 4, 22, 0, 0, 0);
-    Gregorian earth_departure (2033, 8, 13, 0, 0, 0);
+    Gregorian current_gregorian(2019, 4, 22, 0, 0, 0);
+    Gregorian earth_departure(2033, 8, 13, 0, 0, 0);
     Gregorian mars_flyby(2037, 9, 21, 0, 0, 0);
-    Gregorian asteroid_arrival (2039, 7, 13, 0 ,0, 0);
+    Gregorian asteroid_arrival(2039, 7, 13, 0, 0, 0);
 
     double current_date = gregorian_to_julian(current_gregorian);
-    double earth_departure_date =  gregorian_to_julian(earth_departure);
+    double earth_departure_date = gregorian_to_julian(earth_departure);
     double mars_flyby_date = gregorian_to_julian(mars_flyby);
     double asteroid_arrival_date = gregorian_to_julian(asteroid_arrival);
-    
+
     Vector r_0 = Earth.position_at_time(earth_departure_date - current_date);
     Vector r = Mars.position_at_time(mars_flyby_date - current_date);
 
-    LambertResults earth_to_mars = lambert(r_0, r, -1, mars_flyby_date - earth_departure_date);
+    LambertResults earth_to_mars =
+        lambert(r_0, r, -1, mars_flyby_date - earth_departure_date);
 
     r_0 = r;
     r = ToshiH.position_at_time(asteroid_arrival_date - current_date);
 
-    LambertResults mars_to_toshi = lambert(r_0, r, -1, asteroid_arrival_date - mars_flyby_date);
-
+    LambertResults mars_to_toshi =
+        lambert(r_0, r, -1, asteroid_arrival_date - mars_flyby_date);
 
     /* PROBLEM 1D */
     double MARSGM = 9.54953192489925e-11;
@@ -95,24 +70,28 @@ int main() {
     /* PROBLEM 1E */
     double EARTHGM = 8.887692546888129e-10;
     double earth_p = 6371.01;
-    double veloc_sc = (earth_to_mars.v_0 - Earth.velocity_at_time(earth_departure_date - current_date)).norm();
+    double veloc_sc =
+        (earth_to_mars.v_0 -
+         Earth.velocity_at_time(earth_departure_date - current_date))
+            .norm();
     double E = -EARTHGM / sqr(veloc_sc);
     double v_esc_coeff = EARTHGM * ((2 / (1000 * earth_p / AU)) - (1 / E));
     double v_esc = sqrt(v_esc_coeff) * AU / 1000 / 86400;
 
-
     /* PROBLEM 1F */
-    Vector toshi_v = ToshiH.velocity_at_time(asteroid_arrival_date - current_date);
-    double delta_v_m = abs((mars_to_toshi.v_0 - mars_v).norm() - (earth_to_mars.v - mars_v).norm()) * AU / (1000 * 86400);
+    Vector toshi_v =
+        ToshiH.velocity_at_time(asteroid_arrival_date - current_date);
+    double delta_v_m = abs((mars_to_toshi.v_0 - mars_v).norm() -
+                           (earth_to_mars.v - mars_v).norm()) *
+                       AU / (1000 * 86400);
     double delta_v_t = (mars_to_toshi.v - toshi_v).norm() * AU / (1000 * 86400);
     double delta_v = v_esc + delta_v_m + delta_v_t;
 
     /* PROBLEM 2 */
-    Gregorian init_date (2030, 1, 1, 0, 0, 0);
-    Gregorian end_date (2039, 12, 31, 0, 0, 0);
+    Gregorian init_date(2030, 1, 1, 0, 0, 0);
+    Gregorian end_date(2039, 12, 31, 0, 0, 0);
     double init_julian = gregorian_to_julian(init_date) - current_date;
     double end_julian = gregorian_to_julian(end_date) - current_date;
-
 
     for (double i = init_julian; i < end_julian; i += 60) {
         Vector new_E_pos = Earth.position_at_time(i);
@@ -125,11 +104,25 @@ int main() {
                 Vector new_T_veloc = ToshiH.velocity_at_time(k);
                 double delta_t_1 = j - i;
                 double delta_t_2 = k - j;
-                LambertResults v1v2 = lambert(new_E_pos, new_M_pos, -1, delta_t_1);
-                LambertResults v3v4 = lambert(new_M_pos, new_T_pos, -1, delta_t_2);
+                LambertResults v1v2 =
+                    lambert(new_E_pos, new_M_pos, -1, delta_t_1);
+                LambertResults v3v4 =
+                    lambert(new_M_pos, new_T_pos, -1, delta_t_2);
+                Vector e1 = (v1v2.v - new_M_veloc).normalized();
+                Vector e2 = (v3v4.v_0 - new_M_veloc).normalized();
+                Vector e_eps = (e1 - e2).normalized();
 
+                Vector r_peri =
+                    (MARSGM /
+                     ((v1v2.v - new_M_veloc).dot(v1v2.v - new_M_veloc))) *
+                    ((-1 / e_eps.dot(e2)) - 1) * AU / 1000;
 
-            }
+                double rel_veloc = (v1v2.v_0 - new_E_veloc).norm();
+                double dv_EG = sqrt(EARTHGM * ((2 / (1000 * earth_p / AU)) -
+                                               (1 / semi_periapsis))) *
+                               AU / 1000 / 86400;
+
+                if (r_peri) }
         }
     }
 
